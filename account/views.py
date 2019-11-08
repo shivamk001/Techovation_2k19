ec9ed4a78337cb270d2c1d9d9db896e408e69aa0
shivamk001 <shivam.kesarwani001@gmail.com>
2019-11-09 00:17:35 +0530

    first

---
+++ account/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name='account/home.html'
